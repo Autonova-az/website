9967be3b001e8f68fc49feafae20732a2d408ecb
yusifhasanov <yusif.hasanov17@gmail.com>
2025-09-29 17:21:46 +0400

    innitial

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'logos-world.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.vecteezy.com',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'minio.autonova.az',
-        pathname: '/**',
-      },
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'logos-world.net',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'upload.wikimedia.org',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'static.vecteezy.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'minio-dev.autonova.az',
+                pathname: '/autonova/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'minio.autonova.az',
+                pathname: '/autonova/**',
+            },
 
-       {
-        protocol: 'https',
-        hostname: 'minio-dev.autonova.az',
-        pathname: '/**',
-      },
-
-    ],
-  },
+        ],
+    },
 };
 
 export default nextConfig;
